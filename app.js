f614e3f93330def9fcd1c3fa79cc62a650bcbd42
mousa-0 <86013829+mousa-0@users.noreply.github.com>
2021-07-07 12:36:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ import router from "./router";
   const domain = window.env.AUTH0_DOMAIN;
   const client_id = window.env.CLIENT_ID;
   const redirect_uri = window.env.APP_URL;
- 
+  
   window.auth0Client = await createAuth0Client({
     domain,
     client_id,
     redirect_uri,
+    cacheLocation: "localstorage",
+    audience: "https://expenses-api", // 👈 Added
+    scope: "read:reports", // 👈 Added
   });
   
   // handle user navigation
